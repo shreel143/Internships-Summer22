e04894d044e8b2c5e35658002b56e998fe6cee1b
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-14 19:34:15 +0530

    Update 14JUNE_PrePost.c

--- IOT/14JUNE_PrePost.c
+++ IOT/14JUNE_PrePost.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 void main(){
+  
 int a=5;
-a=a++;
-printf("%d",a);//a=5 and then incremented after printing as operator is baadme 
+
+printf("%d",a);//5
+printf("%d",a++);//a=5 and then incremented after printing as operator is baadme 
+printf("%d",a);//a=6 
+  
 int b=5;
-b=++b;
-printf("%d",b);// b=6 first incremented and then output as operator is pahle 
+
+printf("%d",b);//b=5
+printf("%d",++b);// b=6 first incremented and then output as operator is pahle 
+printf("%d",b);// b=6
 }
