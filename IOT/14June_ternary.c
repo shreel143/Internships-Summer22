486bc0df00987c3bcf0bd0472ee785de56d014b0
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-14 19:59:25 +0530

    Create 14June_ternary.c

---
+++ IOT/14June_ternary.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+int a=10;
+int b=12;
+
+int c=a>b?a:b;
+
+printf("%d",c);
+
+a=a+5;
+c=a>b?a:b;
+printf("%d",c);
+
+}
+--------------------------------------------------------------------------
+OPTIMISED APPROACH- (as no extra c of 4 byte is created)
+#include<stdio.h>
+void main()
+{
+int a=10;
+int b=12;
+
+printf("%d",a>b?a:b);
+
+a=a+5;
+printf("%d",a>b?a:b);
+
+}
+
+------------------------------------------------------------------------------
+output-
+  12
+  15
