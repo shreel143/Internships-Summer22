bce464ae619542a9049abcdba47f6f54239abac0
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-14 19:43:18 +0530

    Create 14June_multi_input_output.c

---
+++ IOT/14June_multi_input_output.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a, b,c;
+scanf("%d%d%d",&a,&b,&c);
+//&a means a me field scifier as d so int type me hi jayegi and 
+// variables with& and comma separated 
+// in format specifier without comma in scanf
+  
+printf("a=%d\t b=%d\t c=%d",a,b,c);
+//\t one tab space 
+}
+
+// OUTPUT-
+// 20
+// 30
+// 40
+// a=20     b=30    c=40
