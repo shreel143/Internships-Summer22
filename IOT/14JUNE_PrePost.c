5a01cd926fa5ac7cbc56923410f1cd3b8e958696
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-14 19:27:04 +0530

    Create 14JUNE_PrePost.c

---
+++ IOT/14JUNE_PrePost.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+int a=5;
+a=a++;
+printf("%d",a);//a=5 and then incremented after printing as operator is baadme 
+int b=5;
+b=++b;
+printf("%d",b);// b=6 first incremented and then output as operator is pahle 
+}
