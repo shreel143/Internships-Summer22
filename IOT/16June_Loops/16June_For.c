29e17f7740e7b42a5399fe1d38b377379ee85b86
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-16 21:19:45 +0530

    Create 16June_For.c

---
+++ IOT/16June_Loops/16June_For.c
@@ -0,0 +1,29 @@
+// 1. FOR LOOP
+#include <stdio.h>
+void main()
+{
+    int num,i;
+    printf("Enter the number for which you want to print table of:\n");
+    scanf("%d",&num);
+    
+    printf("TABLE OF %d :\n",num);
+    for(i=1;i<=10;i++)
+    {
+    	printf("%d x %d = %d\n",num,i,num*i);
+	}
+}
+// OUTPUT-
+// Enter the number for which you want to print table of:
+// 18
+// TABLE OF 18 :
+// 18 x 1 = 18
+// 18 x 2 = 36
+// 18 x 3 = 54
+// 18 x 4 = 72
+// 18 x 5 = 90
+// 18 x 6 = 108
+// 18 x 7 = 126
+// 18 x 8 = 144
+// 18 x 9 = 162
+// 18 x 10 = 180
+//----------------------------------------------------------------------------------//
