48e91058bd5bfe0c341cb50e4ca31cb8a0839613
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-17 18:10:53 +0530

    Update 16June.c

--- IOT/16June_Loops/16June.c
+++ IOT/16June_Loops/16June.c
@@ -61,3 +61,24 @@ void main()
 //--------------------------------------------------//
 
 //3. CHAR UPDATION BY USER THROUGH WHILE LOOP
+//3. CHAR UPDATION BY USER THROUGH WHILE LOOP
+#include <stdio.h>
+void main()
+{
+    char per;
+    per = 'y';
+    while (per == 'y') // to compare char use "==" and char value in single ' ' 
+    {
+        printf("\tSUCCESS!! ");
+        printf("\nDo you wish  to continue \n Enter y or n :");
+        scanf("%c", &per);
+    }
+}
+//OUTPUT-
+//         SUCCESS!!
+// Do you wish  to continue
+//  Enter y or n :y
+//         SUCCESS!!
+// Do you wish  to continue
+//  Enter y or n :
+
