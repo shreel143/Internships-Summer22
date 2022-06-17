6c93a300ba8d255aeaee720fc76a4472931e5791
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-17 18:25:20 +0530

    Update 17June.c

--- IOT/17June.c
+++ IOT/17June.c
@@ -1,1 +1,21 @@
-
+// NESTED LOOPS
+#include <stdio.h>
+void main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+	{
+    	for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+    	
+	}
+}
+// OUTPUT-
+// *
+// **
+// ***
+// ****
+// *****
