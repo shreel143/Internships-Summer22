391ebe80d614471a912cd492ec609e0484809966
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-20 19:46:28 +0530

    Update 20June.c

--- IOT/20June.c
+++ IOT/20June.c
@@ -15,7 +15,7 @@ int fact(int x){
 	{
 		return x*fact(x-1);
 	}
-  else if(x==1)
+  else if(x==1) // we have added this extra else if condition because if we get 1 then nothing would be returned so for 1 we would return 1
 	{
 		return 1;
 	}
