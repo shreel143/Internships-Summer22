0fe2fe7163d97c2ea3b00b3be4eb4ec9d1ec03a4
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-22 19:54:15 +0530

    Update 22June.c

--- IOT/22June.c
+++ IOT/22June.c
@@ -23,18 +23,37 @@ void main ()
 }    
 
 //OUTPUT-
-Enter a[0][0]: 56   
-Enter a[0][1]: 10   
-Enter a[0][2]: 30   
-Enter a[1][0]: 34  
-Enter a[1][1]: 21 
-Enter a[1][2]: 34    
-Enter a[2][0]: 45
-Enter a[2][1]: 56
-Enter a[2][2]: 78   
+// Enter a[0][0]: 56   
+// Enter a[0][1]: 10   
+// Enter a[0][2]: 30   
+// Enter a[1][0]: 34  
+// Enter a[1][1]: 21 
+// Enter a[1][2]: 34    
+// Enter a[2][0]: 45
+// Enter a[2][1]: 56
+// Enter a[2][2]: 78   
 
- printing the elements .... 
+//  printing the elements .... 
  
-56      10      30  
-34      21      34  
-45      56      78
+// 56      10      30  
+// 34      21      34  
+// 45      56      78
+
+// 2. POINTERS
+#include <stdio.h>    
+void main ()    
+{    
+    int a=10;
+    int *p=&a;
+    printf("%x",p); //--> printing hexadecimal value i.e address
+    printf("\n%d",p); // as integer
+    printf("\n%u",p);//unsigned 
+    printf("\n%d",p); // as octal form of address
+    printf("\n%d",*p);
+}  
+// OUTPUT-
+ ADDRESS IN HEXADECIMAL FORMAT :61fec8
+ ADDRESS AS INT :6422216
+ ADDRESS AS UNSIGNED INT :6422216
+ ADDRESS IN OCTAL FORM :6422216
+ VALUE OF VARIABLE STORED BY POINTER : 10
