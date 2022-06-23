6fc9b952725803d48681034797f947f06c039a09
Shreel Trivedi <79741191+shreel143@users.noreply.github.com>
2022-06-23 20:13:18 +0530

    Update and rename 23June.c to 23June_Pointers_SizeOf.c

--- IOT/23June_Pointers_SizeOf.c
+++ IOT/23June_Pointers_SizeOf.c
@@ -1,3 +1,4 @@
+// 1. POINTERS ------------------------------------------------------------------------//
 #include<stdio.h>
 
 int main() {
@@ -36,3 +37,31 @@ printf("\nvalue pointed by incremented pointer :%x",*p);
 // value pointed by incremented pointer :0
 // value pointed by incremented pointer :0
 //---------------------------------------------------------------------//
+//2. SIZEOF-----------------------------------------------------------------------------//
+#include<stdio.h>
+
+int main() {
+int a= 25;
+int *p=&a;
+char b='b';
+char *p2=&b;
+int a2[5];
+int a3[5][5];
+
+printf("\n SIZE OF INTEGER VAR : %d",sizeof(a));
+printf("\n SIZE OF INTEGER POINTER : %d",sizeof(p));
+
+printf("\n SIZE OF CHAR VAR %d",sizeof(b));
+printf("\n SIZE OF CHAR POINTER :%d",sizeof(p2));
+
+printf("\n SIZE OF 1D ARRAY i.e. a[5] : %d",sizeof(a2)); //5*4=20
+printf("\n SIZE OF 2D ARRAY i.e. a[5][5] : %d",sizeof(a3)); //5*5*4=20*5=100
+}
+// OUTPUT-
+//  SIZE OF INTEGER VAR : 4
+//  SIZE OF INTEGER POINTER : 8
+//  SIZE OF CHAR VAR 1
+//  SIZE OF CHAR POINTER :8
+//  SIZE OF 1D ARRAY i.e. a[5] : 20
+//  SIZE OF 2D ARRAY i.e. a[5][5] : 100
+//------------------------------------------------------------------------------------//
